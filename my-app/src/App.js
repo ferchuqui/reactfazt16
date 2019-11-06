c655da27f37e3a5b508d91f0a8c862ed455f358a
ferchuqui <luisfernandofarji@hotmail.com>
2019-11-06 12:33:37 -0300

    Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './App.css';
-function HelloWorld() {
-  return(
-    <div id="hello">Hello World</div>
-  )
+function HelloWorld(props) {
+  return (
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
+  );
 }
 
 // const App = () => <div>This is my Component: <HelloWorld/></div>
@@ -17,7 +20,12 @@ function HelloWorld() {
 
 function App() {
   return (
-   <div>This is my Component: <HelloWorld/>  </div>
+    <div>
+      This is my Component:{' '}
+      <HelloWorld mytext="Hola Fernando" subtitle="Component 1" />
+      <HelloWorld mytext="Hola Mundo" subtitle="Component 2" />
+      <HelloWorld mytext="Hello" subtitle="Component 3" />
+    </div>
   );
 }
 
