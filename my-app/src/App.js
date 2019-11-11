48d9bf3c3fbf7c372a2eacd4f0449df2a4975085
ferchuqui <luisfernandofarji@hotmail.com>
2019-11-11 16:13:35 -0300

    fixing troubles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 import './App.css';
 import tasks from './samples/tasks.json';
 import Tasks from '../src/components/Tasks';
+import TaskForm from './components/Taskform';
 class App extends Component {
 state = {
   tasks: tasks
 }
+addTask = () => {
+  console.log('Adding a Task')
+}
 
   render() {
     return <div>
+    <TaskForm />
       <Tasks tasks={this.state.tasks} />
     
     </div>
